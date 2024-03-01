71660ccdac9fbae4b9615111ac7ff62191c1f37e
Stefano D'Annunzio <89990955+stefano-dannunzio@users.noreply.github.com>
2024-03-01 18:20:38 -0300

    Update RpgCardsGameApplication.java

--- rpg_cards_game/src/main/java/com/stefanodannunzio/games/rpg_cards_game/RpgCardsGameApplication.java
+++ rpg_cards_game/src/main/java/com/stefanodannunzio/games/rpg_cards_game/RpgCardsGameApplication.java
@@ -8,6 +8,7 @@ public class RpgCardsGameApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RpgCardsGameApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
